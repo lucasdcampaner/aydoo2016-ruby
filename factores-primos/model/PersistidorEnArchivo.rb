0669ae3b0c64810c621de2e9384f06711c0ceea5
Lucas <lucasdcampaner@gmail.com>
2016-06-07 19:56:03 -0300

    Indentacion en PersistidorEnArchivo.

--- factores-primos/model/PersistidorEnArchivo.rb
+++ factores-primos/model/PersistidorEnArchivo.rb
@@ -1,27 +1,27 @@
 class PersistidorEnArchivo
 
-	@path
-	
-	def initialize(path)     
-    	@path = path    
-  	end
+    @path
+    
+    def initialize(path)     
+        @path = path    
+    end
 
-  	def escribir_factorizacion(factorizacion)
-		
-		File.open(@path, 'w') do |file|
-  			file.puts factorizacion
-		end
-	end
+    def escribir_factorizacion(factorizacion)
+        
+        File.open(@path, 'w') do |file|
+            file.puts factorizacion
+        end
+    end
 
-	def leer_factorizacion()
-		
-		factorizacion = ""
-		File.open(@path, 'r') do |file|
-			while linea = file.gets
-	    		factorizacion = factorizacion + linea 
-	  		end
-		end
-		return factorizacion
-	end
+    def leer_factorizacion()
+        
+        factorizacion = ""
+        File.open(@path, 'r') do |file|
+            while linea = file.gets
+                factorizacion = factorizacion + linea 
+            end
+        end
+        return factorizacion
+    end
 
 end
