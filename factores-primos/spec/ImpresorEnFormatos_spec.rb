fab241cb4503298577d544c802f3c4cb1cce69c8
Lucas <lucasdcampaner@gmail.com>
2016-06-07 20:09:42 -0300

    Agregado de test para ImpresorEnFormatos.

--- factores-primos/spec/ImpresorEnFormatos_spec.rb
+++ factores-primos/spec/ImpresorEnFormatos_spec.rb
@@ -27,5 +27,29 @@ describe 'ImpresorEnFormatos' do
     expect(impresion_esperada).to eq impresion_obtenida
   end
 
+  it 'deberia imprimir "Factores primos 360: 5 3 3 2 2 2 " cuando get_impresion_resultante' do
+    
+    numero_a_factorizar = 360
+    impresor_en_formatos = ImpresorEnFormatos.new()
+    lista_de_factores_primos = [2, 2, 2, 3, 3, 5]
+    impresion_esperada = "Factores primos 360: 5 3 3 2 2 2 "
+
+    impresion_obtenida = impresor_en_formatos.get_impresion_resultante("--format=pretty", numero_a_factorizar, lista_de_factores_primos, "--sort:des")
+    
+    expect(impresion_esperada).to eq impresion_obtenida
+  end
+
+  it 'deberia imprimir "Factores primos 90: 5 3 3 2 " cuando get_impresion_resultante' do
+    
+    numero_a_factorizar = 90
+    impresor_en_formatos = ImpresorEnFormatos.new()
+    lista_de_factores_primos = [2, 3, 3, 5,]
+    impresion_esperada = "Factores primos 90: 5 3 3 2 "
+
+    impresion_obtenida = impresor_en_formatos.get_impresion_resultante("--format=pretty", numero_a_factorizar, lista_de_factores_primos, "--sort:des")
+    
+    expect(impresion_esperada).to eq impresion_obtenida
+  end
+
 end
 
