dab2b95c4da7ebc7080dfae7ae23a510904e58c2
Lucas <lucasdcampaner@gmail.com>
2016-06-07 19:54:24 -0300

    Test de impresion en varias lineas en PersistidorEnArchivo.

--- factores-primos/spec/PersistidorEnArchivo_spec.rb
+++ factores-primos/spec/PersistidorEnArchivo_spec.rb
@@ -15,5 +15,16 @@ describe 'PersistidorEnArchivo' do
     expect(impresion_esperada).to eq impresion_obtenida
   end
 
-end
+  it 'deberia persistir en archivo en varias lineas en el directorio del proyecto cuando escribir_factorizacion' do
+    
+    impresion_esperada = "2\n2\n2\n3\n3\n5 \n"
+    path = "salida.txt"
+    persistidor_en_archivo = PersistidorEnArchivo.new(path)
+    persistidor_en_archivo.escribir_factorizacion(impresion_esperada)
+    
+    impresion_obtenida = persistidor_en_archivo.leer_factorizacion
+    
+    expect(impresion_esperada).to eq impresion_obtenida
+  end
 
+end
