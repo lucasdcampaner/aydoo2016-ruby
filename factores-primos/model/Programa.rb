9121aed57525d43567c05d0558693f00a7d18dee
lcampaner <lucas.campaner@grupoesfera.com.ar>
2016-06-07 17:33:47 -0300

    Clase que contiene la logica del programa.

--- factores-primos/model/Programa.rb
+++ factores-primos/model/Programa.rb
@@ -1,58 +1,62 @@
+require_relative '../model/IdentificadorDeOpciones'
+require_relative '../model/DescomponedorEnFactores'
+require_relative '../model/ImpresorEnFormatos'
+
 class Programa
 
-	@numero_a_factorizar = 0
-	@format = ""
-	@sort = ""
-	@path = ""
-	
+	@numero_a_factorizar
+	@format
+	@sort
+	@path
+
 	def main(argumentos)
 		
 		get_parametros_aplicacion(argumentos)
-		
-		if numero_a_factorizar != 0
-			
-			lista_de_factores_primos = get_lista_de_factores_primos(numero_a_factorizar);
-	    	impresionResultante = get_impresion_resultante(@format, @numero_a_factorizar, lista_de_factores_primos, @sort)
-	    	
-	    	if @pathImpresion == ""
-	    		puts impresion_resultante
+
+		if @numero_a_factorizar != 0
+
+			lista_de_factores_primos = get_lista_de_factores_primos
+	    	impresion_resultante = get_impresion_resultante(lista_de_factores_primos)
+	    	if @path == "sinOutput"
+    			puts impresion_resultante
 	    	else
-	    		persistir_en_archivo(@path, impresion_resultante)
-	    	end	
+	    		persistir_en_archivo(impresion_resultante)
+	    	end
 		else
 			puts "Debe indicar al menos un numero."
 		end
 	end
 	
-	def get_lista_de_factores_primos
-    	
-		descomponedor_en_factores = DescomponedorEnFactores.new
-    	lista_de_factores_primos = descomponedor_en_factores.descomponer_en_factores_primos(@numero_a_factorizar)
-    	return lista_de_factores_primos
-	end
-	
 	def get_parametros_aplicacion(argumentos)
 	
 		identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
-		@numero_a_factorizar = identificador_de_opciones.get_numero_a_factorizar		
-		@format = identificador_de_opciones.get_format
-		@sort = identificador_de_opciones.get_sort
-		@path = identificador_de_opciones.get_output
+		identificador_de_opciones.identificar_opciones()
+		@numero_a_factorizar = identificador_de_opciones.numero_a_factorizar		
+		@format = identificador_de_opciones.format
+		@sort = identificador_de_opciones.sort
+		@path = identificador_de_opciones.output
 	end
-	
+
+	def get_lista_de_factores_primos
+    	
+		descomponedor_en_factores = DescomponedorEnFactores.new
+    	lista_de_factores_primos = descomponedor_en_factores.descomponer_en_factores_primos(@numero_a_factorizar.to_i)
+    	return lista_de_factores_primos
+	end
+
 	def get_impresion_resultante(lista_de_factores_primos) 
 
 		impresor_en_formatos = ImpresorEnFormatos.new
-		impresion_resultante = impresor_en_formatos.get_impresion_resultante(@format, @numero_a_factorizar, lista_de_factores_primos, @sort)
+		impresion_resultante = impresor_en_formatos.get_impresion_resultante(@format, @numero_a_factorizar.to_i, lista_de_factores_primos, @sort)
 		return impresion_resultante
 	end	
-	
+
 	def persistir_en_archivo(impresion_a_persistir) 
 
 		posicion_path_real = 14;
-		path_real = @path[0, posicion_path_real]
+		path_real = @path[posicion_path_real, @path.length]
 		persistidor_en_archivo = PersistidorEnArchivo.new(path_real)
-		persistidor_en_archivo.escribir_factorizacion(impresion_a_persistir)		
+		persistidor_en_archivo.escribir_factorizacion(impresion_a_persistir)
 	end	
 
 end
