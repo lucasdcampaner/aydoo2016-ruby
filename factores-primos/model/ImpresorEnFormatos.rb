2d5626740ef3416c0c1f61feec4207c40491556a
Lucas <lucasdcampaner@gmail.com>
2016-06-06 23:47:23 -0300

    Test para formato quiet.

--- factores-primos/model/ImpresorEnFormatos.rb
+++ factores-primos/model/ImpresorEnFormatos.rb
@@ -10,7 +10,7 @@ class ImpresorEnFormatos
 	    	when "--FORMAT=PRETTY"
 	    		impresion_resultante = imprimir_en_formato_pretty_segun_sort(numero_a_factorizar, lista_de_factores_primos, sort)
 	        when "--FORMAT=QUIET"
-	   
+	   			impresion_resultante = imprimir_en_formato_quiet_segun_sort(lista_de_factores_primos, sort)
 	        else
 	        	impresion_resultante = "Formato no aceptado. Las opciones posibles son: pretty o quiet."
 		end
@@ -29,6 +29,18 @@ class ImpresorEnFormatos
 		
 		return impresion_formato_pretty
 	end
+
+	def imprimir_en_formato_quiet_segun_sort(lista_de_factores_primos, sort) 
+
+		impresion_formato_quiet = ""
+
+		ordenar_lista(lista_de_factores_primos, sort)
+		lista_de_factores_primos.each do |factor|
+			impresion_formato_quiet = impresion_formato_quiet + "#{factor}" + "\n"
+		end
+
+		return impresion_formato_quiet
+	end 	
 
  	def ordenar_lista(lista_de_factores_primos, sort) 
 
