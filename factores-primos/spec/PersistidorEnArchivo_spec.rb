bc543ef68c13bdc0dd29f78fa4932e3e97d8aceb
Lucas <lucasdcampaner@gmail.com>
2016-06-07 00:23:31 -0300

    Primera version de PersistidorEnArchivo.

---
+++ factores-primos/spec/PersistidorEnArchivo_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec' 
+require_relative '../model/PersistidorEnArchivo'
+
+describe 'PersistidorEnArchivo' do
+
+  it 'deberia persistir en archivo en el directorio del proyecto cuando escribir_factorizacion' do
+    
+    impresion_esperada = "Factores primos 90: 2 2 2 3 3 5 \n"
+    path = "salida.txt"
+    persistidor_en_archivo = PersistidorEnArchivo.new(path)
+    persistidor_en_archivo.escribir_factorizacion(impresion_esperada)
+    
+    impresion_obtenida = persistidor_en_archivo.leer_factorizacion
+    
+    expect(impresion_esperada).to eq impresion_obtenida
+  end
+
+end
+
