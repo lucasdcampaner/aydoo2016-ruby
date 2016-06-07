96d47c305de7ca7f66cd7fbb54bfc1878f9e80c5
Lucas <lucasdcampaner@gmail.com>
2016-06-07 19:50:01 -0300

    Refactor e indentacion en la clase Programa.

--- factores-primos/model/Programa.rb
+++ factores-primos/model/Programa.rb
@@ -5,59 +5,64 @@ require_relative '../model/PersistidorEnArchivo'
 
 class Programa
 
-	@numero_a_factorizar
-	@format
-	@sort
-	@path
+    @numero_a_factorizar
+    @format
+    @sort
+    @path
 
-	def main(argumentos)
-		
-		get_parametros_aplicacion(argumentos)
+    def main(argumentos)
+        
+        sin_output = "sinOutput"
+        get_parametros_aplicacion(argumentos)
 
-		if @numero_a_factorizar != 0
+        if @numero_a_factorizar != 0
 
-			lista_de_factores_primos = get_lista_de_factores_primos
-	    	impresion_resultante = get_impresion_resultante(lista_de_factores_primos)
-	    	if @path == "sinOutput"
-    			puts impresion_resultante
-	    	else
-	    		persistir_en_archivo(impresion_resultante)
-	    	end
-		else
-			puts "Debe indicar al menos un numero."
-		end
-	end
-	
-	def get_parametros_aplicacion(argumentos)
-	
-		identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
-		identificador_de_opciones.identificar_opciones()
-		@numero_a_factorizar = identificador_de_opciones.numero_a_factorizar		
-		@format = identificador_de_opciones.format
-		@sort = identificador_de_opciones.sort
-		@path = identificador_de_opciones.output
-	end
+            lista_de_factores_primos = get_lista_de_factores_primos
+            impresion_resultante = get_impresion_resultante(lista_de_factores_primos)
+            if @path == sin_output
+                puts impresion_resultante
+            else
+                persistir_en_archivo(impresion_resultante)
+            end
+        else
+            puts "Debe indicar al menos un numero."
+        end
+    end
 
-	def get_lista_de_factores_primos
-    	
-		descomponedor_en_factores = DescomponedorEnFactores.new
-    	lista_de_factores_primos = descomponedor_en_factores.descomponer_en_factores_primos(@numero_a_factorizar.to_i)
-    	return lista_de_factores_primos
-	end
+    private
+    def get_parametros_aplicacion(argumentos)
+        
+        identificador_de_opciones = IdentificadorDeOpciones.new(argumentos)
+        identificador_de_opciones.identificar_opciones()
+        @numero_a_factorizar = identificador_de_opciones.numero_a_factorizar        
+        @format = identificador_de_opciones.format
+        @sort = identificador_de_opciones.sort
+        @path = identificador_de_opciones.output
+    end
 
-	def get_impresion_resultante(lista_de_factores_primos) 
+    private
+    def get_lista_de_factores_primos
+        
+        descomponedor_en_factores = DescomponedorEnFactores.new
+        lista_de_factores_primos = descomponedor_en_factores.descomponer_en_factores_primos(@numero_a_factorizar.to_i)
+        return lista_de_factores_primos
+    end
 
-		impresor_en_formatos = ImpresorEnFormatos.new
-		impresion_resultante = impresor_en_formatos.get_impresion_resultante(@format, @numero_a_factorizar.to_i, lista_de_factores_primos, @sort)
-		return impresion_resultante
-	end	
+    private
+    def get_impresion_resultante(lista_de_factores_primos) 
 
-	def persistir_en_archivo(impresion_a_persistir) 
+        impresor_en_formatos = ImpresorEnFormatos.new
+        impresion_resultante = impresor_en_formatos.get_impresion_resultante(@format, @numero_a_factorizar.to_i, lista_de_factores_primos, @sort)
+        return impresion_resultante
+    end 
 
-		posicion_path_real = 14;
-		path_real = @path[posicion_path_real, @path.length]
-		persistidor_en_archivo = PersistidorEnArchivo.new(path_real)
-		persistidor_en_archivo.escribir_factorizacion(impresion_a_persistir)
-	end	
+    private
+    def persistir_en_archivo(impresion_a_persistir) 
+
+        posicion_path_real = 14;
+        path_real = @path[posicion_path_real, @path.length]
+        persistidor_en_archivo = PersistidorEnArchivo.new(path_real)
+        persistidor_en_archivo.escribir_factorizacion(impresion_a_persistir)
+    end 
 
 end
